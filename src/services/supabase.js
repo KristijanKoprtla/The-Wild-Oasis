0bc5256e3fc37eeef96d815d4c656cbf60bf2947
BombMarley <kile146@gmail.com>
2023-09-29 18:46:12 +0200

    Connect Supabase with app

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://jykhnmhnhltpkxqmxzjf.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5a2hubWhuaGx0cGt4cW14empmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU4NDIwOTUsImV4cCI6MjAxMTQxODA5NX0.A0SVTN9Pz5NI2-u52E8aUa0xu8734UxJAR8jZfRFYV8";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
