18c809795dfd0913217307440b1798374990d3a2
BombMarley <kile146@gmail.com>
2023-10-07 10:27:40 +0200

    Add SortBy dropdown menu

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
@@ -10,6 +11,19 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "no-discount", label: "No discount" },
           { value: "with-discount", label: "With discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sory by name (A-Z)" },
+          { value: "name-desc", label: "Sory by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sory by price (low to high)" },
+          { value: "regularPrice-desc", label: "Sory by price (high to low)" },
+          { value: "maxCapacity-asc", label: "Sory by capacity (low to high)" },
+          {
+            value: "maxCapacity-desc",
+            label: "Sory by capacity (high to low)",
+          },
         ]}
       />
     </TableOperations>
